5982e1d469ae8ab818e0755c59c06864febef5f9
Marcus <marcussvps2002@gmail.com>
2024-07-24 16:49:49 +0800

    Fixed width of Control Page

--- client/src/pages/Control/Control.js
+++ client/src/pages/Control/Control.js
@@ -7,11 +7,12 @@ import ControlMovementHeader from './ControlMovementHeader'
 const PageContainer = styled('div')({ //Container for the entire page
     background: 'linear-gradient(90deg, #12100e, #2d3436, #434343)',
     minHeight: '100vh',
-    minWidth: '100vw',
+    maxWidth: '100vw',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     padding: 0,
+    overflowX: 'hidden',  // Prevent horizontal scrolling
 });
 
 function TeleOperation(){
